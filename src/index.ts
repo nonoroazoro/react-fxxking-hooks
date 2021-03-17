90f84aeb4c27a1e91893956c4efde95b26d6efaf
Nonoroazoro <xiaochao.k@gmail.com>
2021-03-17 11:28:45 +0800

    Add useToggle

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./useInterval";
 export * from "./useTimeout";
 export * from "./useTimer";
+export * from "./useToggle";
---
+++ src/useToggle.ts
@@ -0,0 +1,16 @@
+import { Reducer, useReducer } from "react";
+
+/**
+ * Toggles boolean state.
+ *
+ * @param {boolean} initialValue Initial value.
+ */
+export function useToggle(initialValue: boolean): [boolean, (nextValue?: boolean) => void]
+{
+    return useReducer<Reducer<boolean, boolean>>(reducer, initialValue);
+}
+
+function reducer(value: boolean, nextValue?: boolean)
+{
+    return nextValue == null ? !value : nextValue;
+}
