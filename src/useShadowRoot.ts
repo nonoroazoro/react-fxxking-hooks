0c16fb835191b7bec8fdfe86ed49d537f30d7cc5
Nonoroazoro <xiaochao.k@gmail.com>
2021-07-09 14:56:42 +0800

    Tweak useShadowRoot

--- src/useShadowRoot.ts
+++ src/useShadowRoot.ts
@@ -6,27 +6,27 @@ import type { RefObject } from "react";
  *
  * If the shadow DOM tree is already there, returns it's shadow root.
  *
- * @param {RefObject<HTMLDivElement>} ref The container to attach the shadow DOM tree. Please note that not all elements can have a shadow DOM tree.
+ * @param {RefObject<Element>} ref The container to attach the shadow DOM tree. Please note that not all elements can have a shadow DOM tree.
  * @param {ShadowRootInit} init The initial options of the shadow root.
  */
-export function useShadowRoot(ref: RefObject<HTMLElement>, init: ShadowRootInit)
+export function useShadowRoot(shadowHostRef: RefObject<Element>, init: ShadowRootInit)
 {
-    const isInitializedRef = useRef(false);
+    const hasAttachedRef = useRef(false);
     const [shadowRoot, setShadowRoot] = useState<ShadowRoot>();
     useLayoutEffect(() =>
     {
-        if (!isInitializedRef.current && ref.current)
+        if (!hasAttachedRef.current && shadowHostRef.current)
         {
-            isInitializedRef.current = true;
-            if (ref.current.shadowRoot)
+            hasAttachedRef.current = true;
+            if (shadowHostRef.current.shadowRoot)
             {
-                setShadowRoot(ref.current.shadowRoot);
+                setShadowRoot(shadowHostRef.current.shadowRoot);
             }
             else
             {
-                setShadowRoot(ref.current.attachShadow(init));
+                setShadowRoot(shadowHostRef.current.attachShadow(init));
             }
         }
-    }, [init, ref]);
+    }, [init, shadowHostRef]);
     return shadowRoot;
 }
